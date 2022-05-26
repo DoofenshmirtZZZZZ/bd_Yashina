7925fe45ce02956b82ca4e655bf81db25156c2e7
DoofenshmirtZZZZZ <90182249+DoofenshmirtZZZZZ@users.noreply.github.com>
2022-05-26 16:52:19 +0300

    Update 2_2_GroupF.sql

--- 2_2_GroupF.sql
+++ 2_2_GroupF.sql
@@ -1,54 +1,60 @@
 /*1.Выведите на экран номера групп и количество студентов, обучающихся в них*/
 
-SELECT groupp, COUNT(*)
-FROM student
-GROUP BY groupp
+SELECT st.group_num, COUNT(st.n_z) FROM students st GROUP BY st.group_num
 
 /* 2.Выведите на экран для каждой группы максимальный средний балл*/
 
-SELECT groupp, MAX(score)
-FROM student
-GROUP BY groupp
+SELECT st.group_num, MAX(st.score) FROM students st GROUP BY st.group_num
 
 /* 3 . Подсчитать количество студентов с каждой фамилией */
 
-SELECT surname, COUNT(surname)
-FROM student
-GROUP BY surname
+SELECT st.surname, COUNT(st.surname) FROM students st GROUP BY st.surname
 
 /* 4 . Подсчитать студентов, которые родились в каждом году*/
 
-SELECT date_birth, count(date_birth)
-FROM student
-GROUP BY date_birth
+SELECT EXTRACT(YEAR FROM st.date_of_birth), COUNT(EXTRACT(YEAR FROM st.date_of_birth)) 
+FROM students st 
+GROUP BY EXTRACT(YEAR FROM st.date_of_birth)
+
+/*5. Для студентов каждого курса подсчитать средний балл*/
+
+SELECT LEFT(st.group_num::VARCHAR,1) grade, ROUND(AVG(st.score),2) score 
+FROM students st 
+GROUP BY LEFT(st.group_num::VARCHAR,1)
+
+/*6. Для студентов заданного курса вывести один номер группы с максимальным средним баллом*/
+
+SELECT st.group_num 
+FROM students st
+WHERE LEFT(st.group_num::VARCHAR,1) = '1'
+GROUP BY st.group_num
+ORDER BY  AVG(st.score) DESC
+LIMIT 1
 
 /* 7  Для каждой группы подсчитать средний балл, вывести на экран только те номера групп и их средний балл, в которых он менее или равен 3.5. Отсортировать по от меньшего среднего балла к большему.*/
 
-SELECT groupp, AVG(score)
-FROM student
-GROUP BY groupp
-HAVING AVG(score)>= 4.5
-ORDER BY AVG(score) ASC
-
-/* У меня нет меньше 3.5 в таблице, взяла 4.5*/
+SELECT st.group_num, ROUND(AVG(st.score),2) score
+FROM students st
+GROUP BY st.group_num
+HAVING AVG(st.score) > 3.5
+ORDER BY  AVG(st.score)
 
 /* 8.Для каждой группы в одном запросе вывести количество студентов, максимальный балл в группе, средний балл в группе, минимальный балл в группе*/
 
-SELECT groupp, COUNT(name),MAX(score),AVG(score),MIN(score)
-FROM student
-GROUP BY groupp
+SELECT st.group_num, COUNT(st.n_z) student_count, MAX(st.score), ROUND(AVG(st.score),2), MIN(st.score)
+FROM students st
+GROUP BY st.group_num
 
 /* 9.Вывести студента/ов, который/ые имеют наибольший балл в заданной группе*/
 
-SELECT name, MAX(score), groupp
-FROM student
-WHERE groupp = 2254
-GROUP BY groupp,name
-HAVING MAX(score) >= 4
+SELECT st.*
+FROM 
+  (SELECT * FROM (SELECT st.group_num, MAX(st.score) FROM students st GROUP BY st.group_num) gr_max
+  WHERE gr_max.group_num = '2255') temp_res, students st
+WHERE temp_res.group_num = st.group_num AND temp_res.max = st.score
 
 /* 10.Аналогично 9 заданию, но вывести в одном запросе для каждой группы студента с максимальным баллом */
 
-SELECT distinct groupp, name ,MAX(score)
-FROM student
-WHERE score = (SELECT MAX(score) from student)
-GROUP BY groupp,name
+SELECT st.*
+FROM (SELECT st.group_num, MAX(st.score) FROM students st GROUP BY st.group_num) gr_max, students st
+WHERE gr_max.group_num = st.group_num AND gr_max.max = st.score
